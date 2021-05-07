ad38140c5754dbeafe126633191ef02fa3ed5761
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-07 17:19:21 +0100

    SpringRunner Class

--- src/test/java/com/harry/demo/category/CategoryTests.java
+++ src/test/java/com/harry/demo/category/CategoryTests.java
@@ -3,8 +3,11 @@ package com.harry.demo.category;
 import com.harry.demo.actor.Actor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CategoryTests {
 
