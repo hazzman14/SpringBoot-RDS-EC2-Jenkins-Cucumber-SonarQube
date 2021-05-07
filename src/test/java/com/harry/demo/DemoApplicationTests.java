aba0792b8f8200f49b3d1a7ec0601b10dc549dbf
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-07 14:41:44 +0100

    First Unit Test

--- src/test/java/com/harry/demo/DemoApplicationTests.java
+++ src/test/java/com/harry/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.harry.demo;
 
+import com.harry.demo.actor.Actor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,19 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldShowSimpleAssertion(){
+		Assertions.assertEquals(1,1);
+	}
+
+	@Test
+	public void testSetFirstName(){
+		Actor actor = new Actor();
+		actor.setFirstName("Harry");
+		Assertions.assertTrue(actor.getFirstName() == "Harry");
+
 	}
 
 }
