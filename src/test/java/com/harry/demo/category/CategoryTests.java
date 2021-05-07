225b4a188d0f868dcc0a2d64bb2dbeaeb51cb0a5
Harry <harryphillipsfacebook@hotmail.co.uk>
2021-05-07 17:23:51 +0100

    Removed SpringRunner

--- src/test/java/com/harry/demo/category/CategoryTests.java
+++ src/test/java/com/harry/demo/category/CategoryTests.java
@@ -3,11 +3,8 @@ package com.harry.demo.category;
 import com.harry.demo.actor.Actor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class CategoryTests {
 
